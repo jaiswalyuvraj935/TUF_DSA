5d1754505023ee8dbbe9380d979b2a3508d66d7c
yuvraj <jaiswalyuvraj935@gmail.com>
2026-07-05 15:13:25 +0530

    optimal

--- ARRAYS/ARRAYS_01/04_remove_duplicate.cpp
+++ ARRAYS/ARRAYS_01/04_remove_duplicate.cpp
@@ -13,21 +13,52 @@ vector<int> method1(vector<int> &arr)
         unique.insert(arr[i]);
     }
 
-    vector<int>result;
-    for(auto it :unique){
+    vector<int> result;
+    for (auto it : unique)
+    {
         result.push_back(it);
     }
 
     return result;
 }
 
+// method 2 using two pointers
+
+int method2(vector<int> &nums)
+{
+
+    if (nums.empty()) {
+        return 0;
+    }
+
+    int k = 1;
+    int n = nums.size();
+    int right = 1;
+    int left = 0;
+
+    while (right < n)
+    {
+        if (nums[left] != nums[right])
+        {
+
+            left++;
+
+            nums[left] = nums[right];
+
+            k++;
+        }
+        right++;
+    }
+    return k;
+}
+
 int main()
 {
     vector<int> arr = {3, 2, 1, 5, 4, 3, 2, 1, 7, 7};
-    vector<int>mainresult = method1(arr);
-    for(auto s:mainresult){
-        cout<< s;
+    vector<int> main_result = method1(arr);
+    for (auto s : main_result)
+    {
+        cout << s;
     }
     return 0;
-    
 }
